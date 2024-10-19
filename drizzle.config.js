f20312268d04d84b76ea3288cdf561baaebebfb2
Mohd Shadab <ms.shadab06@gmail.com>
2024-10-19 16:53:18 +0530

    edit in drizzle

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,7 +4,6 @@ export default {
   schema: "./configs/schema.jsx",
   dialect: "postgresql",
   dbCredentials: {
-    // url: process.env.NEXT_PUBLIC_DB_CONNECTION_STRING,
-    url: "postgresql://neondb_owner:EXP7irTM2bek@ep-withered-pond-a5my1f3q.us-east-2.aws.neon.tech/Ai-Course-Generator?sslmode=require",
+    url: process.env.NEXT_PUBLIC_DB_CONNECTION_STRING,
   },
 };
